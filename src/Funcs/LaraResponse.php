3056d9cff5e9e2915bdb0556c7cb085c4c470a44
siushin <siushin@163.com>
2025-12-12 17:18:01 +0800

    Auto commit at 2025-12-12 17:18:01

--- src/Funcs/LaraResponse.php
+++ src/Funcs/LaraResponse.php
@@ -22,23 +22,23 @@ function success(array $data = [], string $message = 'success', int $code = 200)
 
 /**
  * 抛出HTTP异常
- * @param string   $message
- * @param int|null $error_code
- * @param int|null $code
+ * @param string $message
+ * @param int    $error_code
+ * @param int    $code
  * @return Exception
  * @throws Exception
  * @author siushin<siushin@163.com>
  */
-function throw_exception(string $message = 'fail', int $error_code = null, int $code = null): Exception
+function throw_exception(string $message = 'fail', int $error_code = 0, int $code = 0): Exception
 {
     $is_json_data = json_decode($message, true);
     if (is_array($is_json_data) && isset($is_json_data['message'])) {
         $message = $is_json_data['message'];
     }
-    if ($error_code !== null) {
+    if ($error_code !== 0) {
         $message .= "，错误码：$error_code";
     }
-    $status_code = $code ?? 401;
+    $status_code = $code ?: 401;
     $data = json_encode(compact('code', 'message'), JSON_UNESCAPED_UNICODE);
     throw new Exception($data, $status_code);
 }
@@ -55,11 +55,11 @@ function buildPageData(mixed $data): array
     return [
         'data' => $data['data'] ?? [],
         'page' => [
-            'currentPage' => $data['current_page'] ?? 1,    // 当前页数
+            'currentPage'  => $data['current_page'] ?? 1,    // 当前页数
             'currentCount' => $data['to'] ?: 0,             // 当前记录数
-            'perPage' => (int)($data['per_page'] ?? 10),    // 每页记录数
-            'lastPage' => $data['last_page'] ?? 1,          // 总页数（最后页数）
-            'total' => $data['total'] ?? 0,                 // 总记录数
+            'perPage'      => (int)($data['per_page'] ?? 10),    // 每页记录数
+            'lastPage'     => $data['last_page'] ?? 1,          // 总页数（最后页数）
+            'total'        => $data['total'] ?? 0,                 // 总记录数
         ],
     ];
 }
